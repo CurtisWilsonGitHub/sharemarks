d77a852676be2efc772b082c2ef667ceec44e671
Curtis Wilson <curtiswemail@gmail.com>
2018-07-30 11:06:08 -0600

    Add crud. Add index, new, and show views

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,49 @@
+class TopicsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @topics = Topic.all
+  end
+
+  def show
+    @topic = Topic.find(params[:id])
+  end
+
+  def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = current_user.topics.new(topic_params)
+
+    if @topic.save
+      flash[:notice]="\"#{@topic.title}\" was created."
+      redirect_to @topic
+    else
+      flash.now[:alert]="There was an error saving the topic. Please try again."
+    end
+  end
+
+  def edit
+    @topic = Topic.find(params[:id])
+  end
+
+  def update
+    @topic = Topic.find(params[:id])
+
+    @topic.assign_attributes(topic_params)
+
+    if @topic.save
+        flash[:notice] = "\"#{@topic.title}\" was updated."
+        redirect_to @topic
+    else
+        flash.now[:alert] = "There was an error saving the topic. Please try again"
+        render :edit
+    end
+end
+
+  private
+  def topic_params
+    params.require(:topic).permit(:title)
+  end
+end
